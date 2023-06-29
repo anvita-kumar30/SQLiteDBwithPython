0b29cc943bd02eac365835cfd839625172a04d30
Anvita <kumaranvita30@gmail.com>
2023-06-29 18:24:01 +0530

    init

--- database.py
+++ database.py
@@ -18,14 +18,60 @@ c = conn.cursor()
 # Datatypes:- NULL, INTEGER, REAL, TEXT, BLOB
 
 # Insert into table
-# c.execute("INSERT INTO customers VALUES ('Anvita', 'Kumar', 'anvita@gamil.com')")
-# c.execute("INSERT INTO customers VALUES ('Sam', 'Sparks', 'sam@gamil.com')")
-# c.execute("INSERT INTO customers VALUES ('Mary', 'Brown', 'mary@gamil.com')")
+# c.execute("INSERT INTO customers VALUES ('Anvita', 'Kumar', 'anvita@gmail.com')")
+# c.execute("INSERT INTO customers VALUES ('Sam', 'Sparks', 'sam@gmail.com')")
+# c.execute("INSERT INTO customers VALUES ('Mary', 'Brown', 'mary@gmail.com')")
 
 # many_customers = [('Wes', 'Brown', 'wes@gmail.com'), ('Steph', 'Kuewa', 'steph@gmail.com'), ('Dan', 'Pas', 'dan@gmail.com')]
 # c.executemany("INSERT INTO customers VALUES (?,?,?)", many_customers)
-#
+
 # print("Command executed successfully...")
+
+# Query the DB
+# c.execute("SELECT * FROM customers")
+# print(c.fetchone())
+# print(c.fetchone()[0])
+# print(c.fetchmany(3))
+# print(c.fetchall())
+
+# items = c.fetchall()
+# print(items)
+
+# for item in items:
+# 	print(item)
+# for item in items:
+# 	print(item[0])
+
+# print("NAME" + "\t\tEMAIL")
+# print("-----" + "\t\t--------")
+# for item in items:
+# 	print(item[0] + " " + item[1] + "\t" + item[2])
+
+# c.execute("SELECT rowid, * FROM customers")
+# items = c.fetchall()
+
+# for item in items:
+# 	print(item)
+
+# c.execute("SELECT * FROM customers WHERE last_name = 'Sparks'")
+# items = c.fetchall()
+
+# for item in items:
+# 	print(item)
+
+# c.execute("SELECT * FROM customers WHERE age >= 21")
+
+# c.execute("SELECT * FROM customers WHERE last_name LIKE 'Br%'")
+# items = c.fetchall()
+
+# for item in items:
+# 	print(item)
+
+# c.execute("SELECT * FROM customers WHERE email LIKE '%gmail.com'")
+# items = c.fetchall()
+
+# for item in items:
+# 	print(item)
 
 # Commit our command
 conn.commit()
