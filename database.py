064a5b7eba773c3a2cd83286c7b9e7dabdf41f6d
Anvita <kumaranvita30@gmail.com>
2023-06-29 13:59:00 +0530

    init

--- database.py
+++ database.py
@@ -7,15 +7,25 @@ conn = sqlite3.connect('customer.db')
 c = conn.cursor()
 
 # Create a table
-c.execute("""CREATE TABLE customers (
-	first_name text,
-	last_name text,
-	email text
-	)""")
+# c.execute("""CREATE TABLE customers (
+# 	first_name text,
+# 	last_name text,
+# 	email text
+# 	)""")
 
 # c.execute("CREATE TABLE customers (first_name DATATYPE, last_name DATATYPE, email DATATYPE)")
 
 # Datatypes:- NULL, INTEGER, REAL, TEXT, BLOB
+
+# Insert into table
+# c.execute("INSERT INTO customers VALUES ('Anvita', 'Kumar', 'anvita@gamil.com')")
+# c.execute("INSERT INTO customers VALUES ('Sam', 'Sparks', 'sam@gamil.com')")
+# c.execute("INSERT INTO customers VALUES ('Mary', 'Brown', 'mary@gamil.com')")
+
+# many_customers = [('Wes', 'Brown', 'wes@gmail.com'), ('Steph', 'Kuewa', 'steph@gmail.com'), ('Dan', 'Pas', 'dan@gmail.com')]
+# c.executemany("INSERT INTO customers VALUES (?,?,?)", many_customers)
+#
+# print("Command executed successfully...")
 
 # Commit our command
 conn.commit()
