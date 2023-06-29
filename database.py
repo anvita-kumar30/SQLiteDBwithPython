aa355bf48d9e44d7e380a1d88c5a1164d68698a2
Anvita <kumaranvita30@gmail.com>
2023-06-30 02:12:51 +0530

    init

--- database.py
+++ database.py
@@ -1,189 +1,189 @@
 import sqlite3
 
 # Connect to DB
-# conn = sqlite3.connect(':memory:')
-# conn = sqlite3.connect('customer.db')
+conn = sqlite3.connect(':memory:')
+conn = sqlite3.connect('customer.db')
 
 # Create a cursor
-# c = conn.cursor()
+c = conn.cursor()
 
 # Create a table
-# c.execute("""CREATE TABLE customers (
-# 	first_name text,
-# 	last_name text,
-# 	email text
-# 	)""")
+c.execute("""CREATE TABLE customers (
+	first_name text,
+	last_name text,
+	email text
+	)""")
 
 # c.execute("CREATE TABLE customers (first_name DATATYPE, last_name DATATYPE, email DATATYPE)")
 
 # Datatypes:- NULL, INTEGER, REAL, TEXT, BLOB
 
 # Insert into table
-# c.execute("INSERT INTO customers VALUES ('Anvita', 'Kumar', 'anvita@gmail.com')")
-# c.execute("INSERT INTO customers VALUES ('Sam', 'Sparks', 'sam@gmail.com')")
-# c.execute("INSERT INTO customers VALUES ('Mary', 'Brown', 'mary@gmail.com')")
+c.execute("INSERT INTO customers VALUES ('Anvita', 'Kumar', 'anvita@gmail.com')")
+c.execute("INSERT INTO customers VALUES ('Sam', 'Sparks', 'sam@gmail.com')")
+c.execute("INSERT INTO customers VALUES ('Mary', 'Brown', 'mary@gmail.com')")
 
-# many_customers = [('Wes', 'Brown', 'wes@gmail.com'), ('Steph', 'Kuewa', 'steph@gmail.com'), ('Dan', 'Pas', 'dan@gmail.com')]
-# c.executemany("INSERT INTO customers VALUES (?,?,?)", many_customers)
+many_customers = [('Wes', 'Brown', 'wes@gmail.com'), ('Steph', 'Kuewa', 'steph@gmail.com'), ('Dan', 'Pas', 'dan@gmail.com')]
+c.executemany("INSERT INTO customers VALUES (?,?,?)", many_customers)
 
-# print("Command executed successfully...")
+print("Command executed successfully...")
 
 # Query the DB
-# c.execute("SELECT * FROM customers")
+c.execute("SELECT * FROM customers")
 # print(c.fetchone())
 # print(c.fetchone()[0])
 # print(c.fetchmany(3))
 # print(c.fetchall())
 
-# items = c.fetchall()
-# print(items)
+items = c.fetchall()
+print(items)
 
-# for item in items:
-# 	print(item)
-# for item in items:
-# 	print(item[0])
+for item in items:
+	print(item)
+for item in items:
+	print(item[0])
 
-# print("NAME" + "\t\tEMAIL")
-# print("-----" + "\t\t--------")
-# for item in items:
-# 	print(item[0] + " " + item[1] + "\t" + item[2])
+print("NAME" + "\t\tEMAIL")
+print("-----" + "\t\t--------")
+for item in items:
+	print(item[0] + " " + item[1] + "\t" + item[2])
 
-# c.execute("SELECT rowid, * FROM customers")
-# items = c.fetchall()
+c.execute("SELECT rowid, * FROM customers")
+items = c.fetchall()
 
-# for item in items:
-# 	print(item)
+for item in items:
+	print(item)
 
-# c.execute("SELECT * FROM customers WHERE last_name = 'Sparks'")
-# items = c.fetchall()
+c.execute("SELECT * FROM customers WHERE last_name = 'Sparks'")
+items = c.fetchall()
 
-# for item in items:
-# 	print(item)
+for item in items:
+	print(item)
 
 # c.execute("SELECT * FROM customers WHERE age >= 21")
 
-# c.execute("SELECT * FROM customers WHERE last_name LIKE 'Br%'")
-# items = c.fetchall()
+c.execute("SELECT * FROM customers WHERE last_name LIKE 'Br%'")
+items = c.fetchall()
 
-# for item in items:
-# 	print(item)
+for item in items:
+	print(item)
 
-# c.execute("SELECT * FROM customers WHERE email LIKE '%gmail.com'")
-# items = c.fetchall()
+c.execute("SELECT * FROM customers WHERE email LIKE '%gmail.com'")
+items = c.fetchall()
 
-# for item in items:
-# 	print(item)
+for item in items:
+	print(item)
 
 # Update records
 
-# c.execute("""UPDATE customers SET first_name = 'Bob' WHERE last_name = 'Pas'
-# 	""")
-# conn.commit()
-# c.execute("SELECT * FROM customers")
-# items = c.fetchall()
-# for item in items:
-# 	print(item)
+c.execute("""UPDATE customers SET first_name = 'Bob' WHERE last_name = 'Pas'
+	""")
+conn.commit()
+c.execute("SELECT * FROM customers")
+items = c.fetchall()
+for item in items:
+	print(item)
 
-# c.execute("""UPDATE customers SET first_name = 'Marty' WHERE last_name = 'Brown'
-#  	""")
-# conn.commit()
-# c.execute("SELECT * FROM customers")
-# items = c.fetchall()
-# for item in items:
-# 	print(item)
+c.execute("""UPDATE customers SET first_name = 'Marty' WHERE last_name = 'Brown'
+ 	""")
+conn.commit()
+c.execute("SELECT * FROM customers")
+items = c.fetchall()
+for item in items:
+	print(item)
 
-# c.execute("""UPDATE customers SET first_name = 'Mary' WHERE rowid = 3
-#  	""")
-# conn.commit()
-# c.execute("SELECT * FROM customers")
-# items = c.fetchall()
-# for item in items:
-# 	print(item)
+c.execute("""UPDATE customers SET first_name = 'Mary' WHERE rowid = 3
+ 	""")
+conn.commit()
+c.execute("SELECT * FROM customers")
+items = c.fetchall()
+for item in items:
+	print(item)
 
-# c.execute("""UPDATE customers SET first_name = 'Wes' WHERE rowid = 4
-#  	""")
-# conn.commit()
-# c.execute("SELECT * FROM customers")
-# items = c.fetchall()
-# for item in items:
-# 	print(item)
+c.execute("""UPDATE customers SET first_name = 'Wes' WHERE rowid = 4
+ 	""")
+conn.commit()
+c.execute("SELECT * FROM customers")
+items = c.fetchall()
+for item in items:
+	print(item)
 
-# c.execute("""UPDATE customers SET email = 'mary@gmail.com' WHERE rowid = 3
-#  	""")
-# conn.commit()
-# c.execute("SELECT * FROM customers")
-# items = c.fetchall()
-# for item in items:
-# 	print(item)
+c.execute("""UPDATE customers SET email = 'mary@gmail.com' WHERE rowid = 3
+ 	""")
+conn.commit()
+c.execute("SELECT * FROM customers")
+items = c.fetchall()
+for item in items:
+	print(item)
 
 # Use rowid to update
 
-# c.execute("""UPDATE customers SET first_name = 'Bob' WHERE rowid = '6'
-# 	""")
-# conn.commit()
-# c.execute("SELECT rowid, * FROM customers")
-# items = c.fetchall()
-# for item in items:
-# 	print(item)
+c.execute("""UPDATE customers SET first_name = 'Bob' WHERE rowid = '6'
+	""")
+conn.commit()
+c.execute("SELECT rowid, * FROM customers")
+items = c.fetchall()
+for item in items:
+	print(item)
 
 # Delete records
 
-# c.execute("DELETE from customers WHERE rowid = 6")
-# conn.commit()
-# c.execute("SELECT rowid, * FROM customers")
-# items = c.fetchall()
-# for item in items:
-# 	print(item)
+c.execute("DELETE from customers WHERE rowid = 6")
+conn.commit()
+c.execute("SELECT rowid, * FROM customers")
+items = c.fetchall()
+for item in items:
+	print(item)
 
 # Query the DB - ORDER BY
 
-# c.execute("SELECT rowid, * FROM customers ORDER BY rowid")
-# items = c.fetchall()
-# for item in items:
-# 	print(item)
+c.execute("SELECT rowid, * FROM customers ORDER BY rowid")
+items = c.fetchall()
+for item in items:
+	print(item)
 
-# c.execute("SELECT rowid, * FROM customers ORDER BY rowid DESC")
-# items = c.fetchall()
-# for item in items:
-# 	print(item)
+c.execute("SELECT rowid, * FROM customers ORDER BY rowid DESC")
+items = c.fetchall()
+for item in items:
+	print(item)
 
-# c.execute("SELECT rowid, * FROM customers ORDER BY last_name DESC")
-# items = c.fetchall()
-# for item in items:
-# 	print(item)
+c.execute("SELECT rowid, * FROM customers ORDER BY last_name DESC")
+items = c.fetchall()
+for item in items:
+	print(item)
 
 # AND/OR Clause
 
-# c.execute("SELECT rowid, * FROM customers WHERE last_name LIKE 'Br%' AND rowid = 3")
-# items = c.fetchall()
-# for item in items:
-# 	print(item)
+c.execute("SELECT rowid, * FROM customers WHERE last_name LIKE 'Br%' AND rowid = 3")
+items = c.fetchall()
+for item in items:
+	print(item)
 
-# c.execute("SELECT rowid, * FROM customers WHERE last_name LIKE 'Br%' OR rowid = 3")
-# items = c.fetchall()
-# for item in items:
-# 	print(item)
+c.execute("SELECT rowid, * FROM customers WHERE last_name LIKE 'Br%' OR rowid = 3")
+items = c.fetchall()
+for item in items:
+	print(item)
 
 # Limiting results
 
-# c.execute("SELECT rowid, * FROM customers LIMIT 2")
-# items = c.fetchall()
-# for item in items:
-# 	print(item)
+c.execute("SELECT rowid, * FROM customers LIMIT 2")
+items = c.fetchall()
+for item in items:
+	print(item)
 
-# c.execute("SELECT rowid, * FROM customers ORDER BY rowid DESC LIMIT 2")
-# items = c.fetchall()
-# for item in items:
-# 	print(item)
+c.execute("SELECT rowid, * FROM customers ORDER BY rowid DESC LIMIT 2")
+items = c.fetchall()
+for item in items:
+	print(item)
 
 # Drop table
 
-# c.execute("DROP TABLE customers")
-# conn.commit()
-# c.execute("SELECT rowid, * FROM customers")
-# items = c.fetchall()
-# for item in items:
-# 	print(item)
+c.execute("DROP TABLE customers")
+conn.commit()
+c.execute("SELECT rowid, * FROM customers")
+items = c.fetchall()
+for item in items:
+	print(item)
 
 # Show All Function (Create the table again and insert the same values)
 
@@ -274,9 +274,9 @@ def email_lookup(email):
 	conn.close()
 
 # Commit our command
-# conn.commit()
+conn.commit()
 
 # Close our connection
-# conn.close()
+conn.close()
 
 
