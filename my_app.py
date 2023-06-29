01145bf7e03e9e0338f1e905b46dd4bb13dd5db3
Anvita <kumaranvita30@gmail.com>
2023-06-30 02:09:57 +0530

    init

--- my_app.py
+++ my_app.py
@@ -1,21 +1,21 @@
 import database
 
 # Add a record to the DB
-# database.add_one("Laura","Smith","laura@smith.com")
+database.add_one("Laura","Smith","laura@smith.com")
 
 # Delete a record from the table (use rowid as string)
-# database.delete_one('6')
+database.delete_one('6')
 
 # Add many records to table
-# stuff = [
-# 	('Brenda', 'Smitherton', 'brenda@gmail.com'),
-# 	('Joshua', 'Rain', 'joshua@gmail.com')
-# 	]
-# database.add_many(stuff)
+stuff = [
+	('Brenda', 'Smitherton', 'brenda@gmail.com'),
+	('Joshua', 'Rain', 'joshua@gmail.com')
+	]
+database.add_many(stuff)
 
 # Lookup email address record
-# database.email_lookup("anvita@gmail.com")
+database.email_lookup("anvita@gmail.com")
 
 # Show all the records
-# database.show_all()
+database.show_all()
 
